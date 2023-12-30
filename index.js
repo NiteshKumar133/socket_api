29938295b71407f1626ddc2e3e80c92fc5b5b8b1
NiteshKumar133 <135575205+NiteshKumar133@users.noreply.github.com>
2023-12-30 23:21:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const io = require("socket.io")({
-    cors: {
-      origin: "http://127.0.0.1:5500", 
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
-const users = {};
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+const http=require("http");
+const express =require("express");
+const cors = require("cors");
+const socketIO = require("socket.io");
 
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
-    });
+const app=express();
+const port= process.env.PORT||3001 ;
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('left',users[socket.id]);
-        delete users[socket.id];
-    
-    });
+
+const users=[{}];
+
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+})
+
+const server=http.createServer(app);
+
+const io=socketIO(server);
+
+io.on("connection",(socket)=>{
+    console.log("New Connection");
+
+    socket.on('joined',({user,id})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",data:` ${users[socket.id]} has joined`,id:id});
+          socket.emit('welcome',{user:"Admin",data:`Welcome to the chat`,id:id})
+    })
+    socket.on('stream', (stream) => {
+        // Broadcast the stream to other connected users
+        socket.broadcast.emit('stream', stream);
+      });
+     socket.on('msg',({data,id,time,seen, image})=>{
+        io.emit('sendMessage',{user:users[id],data,id,time:time,seen:seen,image:image});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",data:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+    })
 });
-io.listen(process.env.PORT||8000); 
+
+
+server.listen(port,()=>{
+    console.log(`Working`);
+})
